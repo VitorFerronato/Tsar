ad9f7c98bd0f760a4ab9c21c488172e69c2b93a9
Vitor Ferronato <vitorferronato@gmail.com>
2022-02-16 21:45:51 -0300

    Create Content

--- js/script.js
+++ js/script.js
@@ -1,44 +1,27 @@
-// Scroll Suave
-const menuLinks = document.querySelectorAll('.main-ul a');
 
-function getDistanceFromTheTop(element) {
-  const id = element.getAttribute("href");
-  return document.querySelector(id).offsetTop;
-}
+// MENU HAMBUERGUER --------------
 
+document.addEventListener('DOMContentLoaded', () => {
 
-function scrollToSection(event) {
-  event.preventDefault();
-  const distanceFromTheTop = getDistanceFromTheTop(event.target) - 90;
-  smoothScrollTo(0, distanceFromTheTop);
-}
-
-menuLinks.forEach((link) => {
-  link.addEventListener("click", scrollToSection);
-});
-
-function smoothScrollTo(endX, endY, duration) {
-  const startX = window.scrollX || window.pageXOffset;
-  const startY = window.scrollY || window.pageYOffset;
-  const distanceX = endX - startX;
-  const distanceY = endY - startY;
-  const startTime = new Date().getTime();
-
-  duration = typeof duration !== "undefined" ? duration : 600;
-
-  const easeInOutQuart = (time, from, distance, duration) => {
-    if ((time /= duration / 2) < 1)
-      return (distance / 2) * time * time * time * time + from;
-    return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
-  };
-
-  const timer = setInterval(() => {
-    const time = new Date().getTime() - startTime;
-    const newX = easeInOutQuart(time, startX, distanceX, duration);
-    const newY = easeInOutQuart(time, startY, distanceY, duration);
-    if (time >= duration) {
-      clearInterval(timer);
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+  
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+  
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+  
+        });
+      });
     }
-    window.scroll(newX, newY);
-  }, 1000 / 60);
-}
+  });
